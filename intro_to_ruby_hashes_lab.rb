92ac110497ddcd06c12def153453047eccb654a8
Scott Espinosa <scottjames.espinosa@gmail.com>
2020-05-10 07:30:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ require 'pry'
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-  monopoly_square = { :railroad => {} }
+  monopoly_square = { :railroads => {} }
 end
 
 def monopoly_with_second_tier
